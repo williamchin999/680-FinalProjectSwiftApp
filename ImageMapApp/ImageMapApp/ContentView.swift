b4e7dac6733fc2ecd2cbfa64a1d427b59fab9f9c
williamchin999 <williamchin999@gmail.com>
2022-11-08 19:45:02 -0800

    added project

---
+++ ImageMapApp/ImageMapApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ImageMapApp
+//
+//  Created by William Chin on 11/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ImageMapApp/ImageMapApp/ImageMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageMapAppApp.swift
+//  ImageMapApp
+//
+//  Created by William Chin on 11/8/22.
+//
+
+import SwiftUI
+
+@main
+struct ImageMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
